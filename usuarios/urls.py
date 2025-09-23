3f0ac9e9182723cb13b449bac8d3edebf024603f
César Gehres <cgehres@imply.com>
2025-09-23 17:44:43 -0300

    feat: adicionado end point para registrar medidas do usuário.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('atualizar_status_exercicio/', views.atualizar_status_exercicio_view, name='atualizar_status_exercicio'),
     path('atualizar_status_treino/', views.atualizar_usuario_treino_view, name='atualizar_status_treino'),
     path('inscrever_usuario_treino/', views.inscrever_usuario_treino_view, name='inscrever_usuario_treino'),
-    path('registrar_historico_usuario/', views.criar_historico_usuario_view, name='registrar_historico_usuario')
+    path('registrar_historico_usuario/', views.criar_historico_usuario_view, name='registrar_historico_usuario'),
+    path('listar_historico_usuario', views.listar_historico_usuario_view, name='listar_historico_usuario')
 ]
