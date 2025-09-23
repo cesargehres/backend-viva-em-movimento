c6ee52d11fcff34a51f7d50d7942fbd4d8293397
César Gehres <cgehres@imply.com>
2025-09-23 17:46:59 -0300

    fix: estava falantando "/" no fim da rota listar_historico_usuario.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('atualizar_status_treino/', views.atualizar_usuario_treino_view, name='atualizar_status_treino'),
     path('inscrever_usuario_treino/', views.inscrever_usuario_treino_view, name='inscrever_usuario_treino'),
     path('registrar_historico_usuario/', views.criar_historico_usuario_view, name='registrar_historico_usuario'),
-    path('listar_historico_usuario', views.listar_historico_usuario_view, name='listar_historico_usuario')
+    path('listar_historico_usuario/', views.listar_historico_usuario_view, name='listar_historico_usuario')
 ]
