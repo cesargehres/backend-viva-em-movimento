c061a98597f711a84981fd987583727d37371a49
César Gehres <cgehres@imply.com>
2025-09-24 15:07:41 -0300

    feat: adicionado endopoint ver_exercio

--- viva_em_movimento/settings.py
+++ viva_em_movimento/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',  # ativa autenticação por token
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
